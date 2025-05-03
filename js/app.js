c33db4bafeabea3f53ed8aed0ae22cc9ba631ea3
mahmoodalawi <mahmoodbh19@email.com>
2025-05-03 23:33:33 +0300

    To verify that element are recognized

--- js/app.js
+++ js/app.js
@@ -15,6 +15,10 @@ let tie;
 const squareEls = document.querySelectorAll('.sqr');
 const messageEl = document.getElementById('message');
 
+// To verify that element are recognized 
+// console.log(squareEls);
+// console.log(messageEl);
+
 /*-------------------------------- Functions --------------------------------*/
 
 
