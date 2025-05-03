4a0354a89d2ff78c7417f25c063f250b5316aa27
mahmoodalawi <mahmoodbh19@email.com>
2025-05-03 23:30:23 +0300

    Store cached element references

---
+++ js/app.js
@@ -0,0 +1,25 @@
+/*-------------------------------- Constants --------------------------------*/
+
+
+
+/*---------------------------- Variables (state) ----------------------------*/
+
+let board;
+let turn;
+let winner;
+let tie;
+
+
+/*------------------------ Cached Element References ------------------------*/
+
+const squareEls = document.querySelectorAll('.sqr');
+const messageEl = document.getElementById('message');
+
+/*-------------------------------- Functions --------------------------------*/
+
+
+
+/*----------------------------- Event Listeners -----------------------------*/
+
+
+
