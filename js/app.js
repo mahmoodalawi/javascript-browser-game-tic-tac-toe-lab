59dd8deb5eb90a53a09f971a9a63373058303b0d
mahmoodalawi <mahmoodbh19@email.com>
2025-05-04 19:37:24 +0300

    Create winningCombos array

--- js/app.js
+++ js/app.js
@@ -1,6 +1,15 @@
 /*-------------------------------- Constants --------------------------------*/
-
-
+const winningCombos = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [2, 4, 6],
+    [6, 7, 8],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [0, 3, 6],
+    
+  ];
 
 /*---------------------------- Variables (state) ----------------------------*/
 
@@ -46,6 +55,11 @@ const updateBoard = () => {
       squareEls[index].textContent = valueXO;
     };
   }; 
+
+ const winningCombos =() =>{
+
+
+ }
 
   const render = () => {
     updateBoard();
