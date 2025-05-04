59f37faf90187c7bacd0325a528e963c4b09ac7a
mahmoodalawi <mahmoodbh19@email.com>
2025-05-04 21:28:57 +0300

    Calling checkWinner

--- js/app.js
+++ js/app.js
@@ -74,12 +74,37 @@ const updateBoard = () => {
      }
     // console.log('Clicked square index:', squareIndex);
     placePiece(squareIndex);
+    checkForWinner();
 
   };
 
-  const checkForWinner = () => {
-    
+  const checkWinner = () => {
+    if (board[0] !== '' && board[0] === board[1] && board[0] === board[2]) {
+      winner = true;
+    }
+    if (board[3] !== '' && board[3] === board[4] && board[3] === board[5]) {
+      winner = true;
+    }
+    if (board[2] !== '' && board[2] === board[4] && board[2] === board[6]) {
+      winner = true;
+    }
+    if (board[6] !== '' && board[6] === board[7] && board[6] === board[8]) {
+      winner = true;
+    }
+    if (board[1] !== '' && board[1] === board[4] && board[1] === board[7]) {
+      winner = true;
+    }
+    if (board[2] !== '' && board[2] === board[5] && board[2] === board[8]) {
+      winner = true;
+    }
+    if (board[0] !== '' && board[0] === board[4] && board[0] === board[8]) {
+      winner = true;
+    }
+    if (board[0] !== '' && board[0] === board[3] && board[0] === board[6]) {
+      winner = true;
+    }
   };
+  
 
   const render = () => {
     updateBoard();
